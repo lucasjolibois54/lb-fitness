4c6658851509057f67a62289ca71ee72aece4113
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-23 00:00:29 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp
