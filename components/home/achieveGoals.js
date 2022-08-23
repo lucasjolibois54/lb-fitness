4972ab0e752b5b0e46564e34e5866b12a5757714
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 00:24:09 +0200

    Update achieveGoals.js

--- components/home/achieveGoals.js
+++ components/home/achieveGoals.js
@@ -5,7 +5,7 @@ const achieveGoals = () => {
         <>
         <div className='flex justify-center'>
             <div class="relative max-w-8xl">
-                <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-10 text-2xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
+                <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
                     Achieve your training <br />
                     goals with the right plan, <br />
                     not with luck.</h2>
