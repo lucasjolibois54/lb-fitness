88fe54b980dbc4079e5ea8a3ffbdd724cf68e317
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-23 21:40:52 +0200

    Update achieveGoals.js

--- components/home/achieveGoals.js
+++ components/home/achieveGoals.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const achieveGoals = () => {
     return (
-        <div>
-            <div class="relative">
+        <>
+        <div className='flex justify-center'>
+            <div class="absolute max-w-8xl">
                 <img className='w-full' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png" />
-                <h2 class="absolute text-5xl font-bold leading-tight text-white top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
+                <h2 class="!leading-tight absolute flex items-center justify-center sm:text-5xl font-bold text-white top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
                     Achieve your training <br />
                     goals with the right plan, <br />
                     not with luck.</h2>
@@ -14,6 +15,7 @@ const achieveGoals = () => {
                 <h3 class="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
             </div>
         </div>
+        </>
     )
 }
 
