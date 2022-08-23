02eb19f26da47e517524ab9382ff6c0e79b2edff
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-23 18:24:07 +0200

    Create achieveGoals.js

---
+++ components/home/achieveGoals.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const achieveGoals = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center'>
+            <img
+                className="w-full  xl:w-6/6"
+                src={
+                    "https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png"
+                }
+                alt="Hero Image"
+            />
+            </div>
+        </div>
+    )
+}
+
+export default achieveGoals
+
+
