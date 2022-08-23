fabebe7b80e0545e64edf83d9704b96ac10c5fb9
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 01:13:00 +0200

    Update achieveGoals.js

--- components/home/achieveGoals.js
+++ components/home/achieveGoals.js
@@ -3,18 +3,18 @@ import React from 'react'
 const achieveGoals = () => {
     return (
         <>
-        <div className='flex justify-center'>
-            <div class="relative max-w-8xl">
-                <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
-                    Achieve your training <br />
-                    goals with the right plan, <br />
-                    not with luck.</h2>
-                <img className='w-full' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png" />
-                {/* <h2 class="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
+            <div className='flex justify-center'>
+                <div class="relative max-w-8xl">
+                    <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
+                        Achieve your training <br />
+                        goals with the right plan, <br />
+                        not with luck.</h2>
+                    <img className='w-full' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png" />
+                    {/* <h2 class="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
                 <h3 class="absolute text-2xl text-blue-300 top-5 left-5">Top Left</h3>
                 <h3 class="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
+                </div>
             </div>
-        </div>
         </>
     )
 }
