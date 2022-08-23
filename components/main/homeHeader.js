67326a50bed08b40d68d5633a62bc084ca73fad2
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 01:10:18 +0200

    Create homeHeader.js

---
+++ components/main/homeHeader.js
@@ -0,0 +1,12 @@
+
+
+function homeHeader() {
+  return (
+    <div className='max-w-7xl mx-auto font-bold'>
+      <p className='float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-12'>LB│FITNESS</p>
+      <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/login">Login</a>
+      </div>
+  )
+}
+
+export default homeHeader
