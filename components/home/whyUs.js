f390b3d8cc686d190e15b0b1daa89c9f990568f7
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 00:25:00 +0200

    Update whyUs.js

--- components/home/whyUs.js
+++ components/home/whyUs.js
@@ -1,26 +1,6 @@
 
 function whyUs() {
     return (
-        /*     <div id="about" className="mb-28 p-0 flex flex-col w-full xl:flex-row md:p-16 md:mb-18">
-             <div  className="w-full xl:w-full">
-     <img src="https://res.cloudinary.com/dckwf6med/image/upload/v1661290332/Web%20Dev%20Projects/Group_74_wolzcr.png" className="pt-6" alt="church"/>
-     </div>
-     <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
-     
-     <h2   className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
-     Hør mere om  <br />
-     <span className="text-orange-text"> hvem vi er!</span>
-     </h2>
-     <p  className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
-     Ideen bag Boravio kom fra et behov om at stoppe spild, samt reducering af køb af engangsbrug. Mange folk kender det at købe ting der kun bruges få gange om året, som bare sidder og samler støv i kælderen. Ting som værktøjer, kufferter, og andre sæson genstande. Det virker derfor som en oplagte ide at man bare låner de ting, som man kun bruger få gange, det kan desværre blev meget dyrt i længden.
-     </p>
-     <div className="flex justify-center text-center items-center py-0 xl:justify-start">
-     
-     
-             
-     
-         </div></div></div>*/
-
 
         <div className="flex flex-col-reverse md:flex-row">
             <div className="mx-auto float-right">
@@ -38,61 +18,6 @@ function whyUs() {
             </div>
         </div>
 
-
-
-
-/*
-<div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 lg:text-left lg:pl-10">
-<h2 className="text-white mt-7 sm:mt-0 text-4xl xsm:text-4xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-3xl xsm:text-3xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2>
-    <p className="text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
-    </p>
-    <div className="mt-4">
-        <a className="text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
-            Explore
-        </a></div>
-
-</div>*/
-
-
-
-
-
-
-
-
-
-
-
-
-        /*<div>
-            <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-                <div
-                    id="top"
-                    className=" p-0 flex flex-col-reverse w-full lg:flex-row md:p-16 mb-4">
-                    <div className="w-full  xl:w-4/6 mt-10 sm:mt-0">
-                        <img
-                            className=" pl-5 pr-5 h-auto w-auto my-auto lg:mt-20"
-                            src={
-                                'https://res.cloudinary.com/dckwf6med/image/upload/v1661290332/Web%20Dev%20Projects/Group_74_wolzcr.png'
-                            }
-                            alt="mockup"
-                        />
-                    </div>
-                    <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 lg:text-left lg:pl-10">
-                    <h2 className="text-white mt-7 sm:mt-0 text-4xl xsm:text-4xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-3xl xsm:text-3xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2>
-                        <p className="text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
-                        </p>
-                        <div className="mt-4">
-                            <a className="text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
-                                Explore
-                            </a></div>
-
-                    </div>
-                </div>
-            </div>
-        </div>*/
     )
 }
 
