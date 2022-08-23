8e3395f3b34eb3722186de3c774bfad55b74b2f2
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-23 02:57:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //background colors
+        'pink': '#C03963',
+        'gray-stroke': '#7E7D7D'}
+    },
   },
   plugins: [],
 }
