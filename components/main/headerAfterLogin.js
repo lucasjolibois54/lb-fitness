89f09f597136637f4548d86fe2df5b106e82af15
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 17:15:30 +0200

    Update headerAfterLogin.js

--- components/main/headerAfterLogin.js
+++ components/main/headerAfterLogin.js
@@ -2,7 +2,7 @@
 
 function headerAfterLogin() {
     return (
-      <div className='max-w-7xl mx-auto font-bold'>
+      <div className=' zzz max-w-7xl mx-auto font-bold'>
         <p className='float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-12'>LB│FITNESS</p>
         <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</a>
         </div>
