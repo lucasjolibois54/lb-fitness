16c9de3eadd0b834f55cba0055f4915faefc1d7d
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 20:15:13 +0200

    Update introduction.js

--- pages/membersonly/introduction.js
+++ pages/membersonly/introduction.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import HeaderAfterLogin from '../../components/main/headerAfterLogin';
 import Hero from '../../components/introcution/hero';
+import SubHero from '../../components/introcution/subHero';
+import Footer from '../../components/main/footer';
 
 function introduction() {
   return (
     <div>
         <HeaderAfterLogin/>
         <Hero/>
+        <SubHero/>
+        <Footer/>
         
         </div>
   )
