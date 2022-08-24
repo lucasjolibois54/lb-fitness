3fec645806cf8f023b7e8bf17568fa4f51ee3c5d
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 22:59:58 +0200

    Update homeHeader.js

--- components/main/homeHeader.js
+++ components/main/homeHeader.js
@@ -3,7 +3,7 @@
 function homeHeader() {
   return (
     <div className='max-w-7xl mx-auto font-bold'>
-      <p className='float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-12'>LB│FITNESS</p>
+      <p href="/" className='float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-12'><a href="/">LB│FITNESS</a></p>
       <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/login">Login</a>
       </div>
   )
