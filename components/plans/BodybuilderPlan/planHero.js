05697804cff9760896e1c94a17187999696b8140
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 17:22:04 +0200

    Update planHero.js

--- components/plans/BodybuilderPlan/planHero.js
+++ components/plans/BodybuilderPlan/planHero.js
@@ -3,7 +3,8 @@ function planHero() {
     <>
     <div className='flex justify-center sm:h-screen'>
         <div class="relative max-w-8xl">
-            <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:!text-8xl stroke-removal font-bold text-white top-1/2 lg:bottom-0 lg:mr-10 right-1/3 translate-x-2/3 translate-y-1/3">
+            <h2 class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-5xl xsm:text-4
+            5xl sm:text-4xl md:lg:text-6xl lg:!text-8xl stroke-removal font-bold text-white top-1/2 lg:bottom-0 lg:mr-10 sm:right-1/3 sm:translate-x-2/3 sm:translate-y-1/3">
             <span className="stroke-title">READY TO</span><br/> GET STARTED <br /> <span className="stroke-title">AT LB FITNESS</span>
                 </h2>
             <img className='bg h-screen w-screen' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661351404/Web%20Dev%20Projects/Group_76_6_mku7rr.png" />
