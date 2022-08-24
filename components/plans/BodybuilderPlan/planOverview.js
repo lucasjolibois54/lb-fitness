0b46418c8cce5c9f8852d7b50d5a184da5852079
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 18:02:32 +0200

    Update planOverview.js

--- components/plans/BodybuilderPlan/planOverview.js
+++ components/plans/BodybuilderPlan/planOverview.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function planOverview() {
   return (
-    <div>planOverview</div>
+    <div>
+        <div class="grid grid-rows-4 grid-flow-col gap-4">
+  <div>01</div>
+  <div>01</div>
+  <div>01</div>
+  
+  <div>09</div>
+</div>
+    </div>
   )
 }
 
