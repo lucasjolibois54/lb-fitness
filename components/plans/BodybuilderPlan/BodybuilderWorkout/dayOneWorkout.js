6d8369895f15f2121de7a2d7c4cdc1fc243d97cb
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 22:54:24 +0200

    Update dayOneWorkout.js

--- components/plans/BodybuilderPlan/BodybuilderWorkout/dayOneWorkout.js
+++ components/plans/BodybuilderPlan/BodybuilderWorkout/dayOneWorkout.js
@@ -5,7 +5,7 @@ function dayOneWorkout(props) {
     <div className="max-w-6xl mx-auto center-this">
       <div className=" flex flex-col mt-20 sm:flex-row w-10/12 sm:w-10/12 md:w-10/12 lg:w-9/12 p-3 sm:p-10 border-gray-200 border-2 rounded-2xl">
         <div className="flex flex-col sm:pr-5">
-          <h2 className="pt-5 text-center sm:text-left font-bold text-5xl sm:text-6xl md:text-7xl">
+          <h2 className="pt-5 text-center sm:text-left font-bold text-2xl sm:text-3xl md:text-4xl">
             {props.name}
           </h2>
           <p className="max-w-xl text-sm text-center sm:text-left md:text-md text-gray-400 mt-5">
