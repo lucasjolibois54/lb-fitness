3d0baeee1f1402ec3df0fe99dbe7bf30c32d2327
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 20:54:37 +0200

    Update bodybuilderPlan.js

--- pages/membersonly/bodybuilderPlan.js
+++ pages/membersonly/bodybuilderPlan.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import PlanOverview from '../../components/plans/BodybuilderPlan/planOverview';
+import HeaderAfterLogin from '../../components/main/headerAfterLogin';
+import Hero from '../../components/plans/BodybuilderPlan/BodybuilderWorkout/hero';
 
 function members() {
   return (
     <div>
-        <PlanOverview/>
+        <HeaderAfterLogin/>
+        <Hero/>
+        Bodybuilder plan
     </div>
   )
 }
