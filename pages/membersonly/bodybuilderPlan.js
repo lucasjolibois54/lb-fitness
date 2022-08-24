364d55fd976cc20c5cfdf185c7d0ee2657eaa7f8
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-24 22:09:10 +0200

    Update bodybuilderPlan.js

--- pages/membersonly/bodybuilderPlan.js
+++ pages/membersonly/bodybuilderPlan.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import PlanOverview from '../../components/plans/BodybuilderPlan/planOverview';
-import HeaderAfterLogin from '../../components/main/headerAfterLogin';
-import Hero from '../../components/plans/BodybuilderPlan/BodybuilderWorkout/hero';
-import Footer from '../../components/main/footer';
+import React from "react";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import PlanOverview from "../../components/plans/BodybuilderPlan/planOverview";
+import HeaderAfterLogin from "../../components/main/headerAfterLogin";
+import Hero from "../../components/plans/BodybuilderPlan/BodybuilderWorkout/hero";
+import BodybuilderDays from "../../components/plans/BodybuilderPlan/BodybuilderWorkout/bodybuilderDays";
+import Footer from "../../components/main/footer";
 
 function members() {
   return (
     <div>
-        <HeaderAfterLogin/>
-        <Hero/>
-        <Footer/>
+      <HeaderAfterLogin />
+      <Hero />
+      <BodybuilderDays
+        name="Day 1"
+        description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
+        image="https://res.cloudinary.com/dckwf6med/image/upload/v1661370819/Web%20Dev%20Projects/Rectangle_12_2_1_gdmvdj.webp"
+        linking="/"
+      />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default members
+export default members;
 
 export const getServerSideProps = withPageAuthRequired();
