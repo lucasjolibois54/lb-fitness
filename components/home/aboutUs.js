28a91a9207e37ab832154484167f193c0fe2f81c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 22:51:05 +0200

    Update aboutUs.js

--- components/home/aboutUs.js
+++ components/home/aboutUs.js
@@ -19,39 +19,6 @@ function aboutUs() {
                 </div>
             </div>
 
-
-            {/* <div>
-                <div id="about" className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-                    <div id="top" className=" p-0 flex flex-col w-full md:flex-row md:p-16 mb-4">
-                        <div className="flex text-center flex-col w-full px-3 justify-center md:w-5/10 md:text-left lg:pl-10">
-
-                            <h2 className="text-white mt-7 sm:mt-0 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2>
-                            <p className="text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
-                                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
-                            </p>
-                            <div className="mt-4">
-                                <a className="text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
-                                    Explore
-                                </a></div>
-                        </div>
-                        <div className="w-4/6  xl:w-4/6">
-                            <img
-                                className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto mt-20 mx-auto"
-                                src={
-                                    "https://res.cloudinary.com/dckwf6med/image/upload/v1661290332/Web%20Dev%20Projects/Group_74_wolzcr.png"
-                                }
-                                alt="mockup"
-                            />
-                        </div>
-                    </div>
-
-                </div>
-            </div> */}
-
-
-
-
-
         </>
     )
 }
