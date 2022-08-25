6e2b8c60015274aef157730d9975ca816e867d89
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 22:53:16 +0200

    Update aboutUs.js

--- components/home/aboutUs.js
+++ components/home/aboutUs.js
@@ -8,7 +8,7 @@ function aboutUs() {
                     <img className="w-11/12" src="https://res.cloudinary.com/dckwf6med/image/upload/v1661295966/Web%20Dev%20Projects/Group_75_1_yyn9db.png" />
                 </div>
                 <div className="text-center md:text-left mx-10 md:mx-0 mb-10 md:mb-0 flex flex-col justify-center">
-                    <h2 className="text-white mt-7 sm:mt-0 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2>
+                    <h2 className="text-white mt-7 sm:mt-0 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">LEARN MORE <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> ABOUT US  </span></h2>
                     <p className="text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
                         It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
                     </p>
