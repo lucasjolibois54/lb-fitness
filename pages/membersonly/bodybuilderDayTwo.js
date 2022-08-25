3e8b887da133fbe0fa809109bc0c57421dfe9d8f
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 19:53:56 +0200

    Update bodybuilderDayTwo.js

--- pages/membersonly/bodybuilderDayTwo.js
+++ pages/membersonly/bodybuilderDayTwo.js
@@ -21,37 +21,37 @@ function bodyBuilderDayTwo() {
         name="#1 Barbell Squat"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661378417/Web%20Dev%20Projects/AnyConv.com__Rectangle_744_1_qdanjm.webp"
-        sets="5 sets of 5-12 reps"
+        sets="3-4 sets of 5-12 reps"
       />
       <DayOneWorkout
         name="#2 Leg Extension"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661377053/Web%20Dev%20Projects/Rectangle-740_ez2shi.webp"
-        sets="5 sets of 5-12 reps"
+        sets="4 sets of 5-12 reps"
       />
       <DayOneWorkout
         name="#3 Leg press"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661377389/Web%20Dev%20Projects/Rectangle-741-_1__kjn1fx.webp"
-        sets="5 sets of 5-12 reps"
+        sets="4 sets of 5-12 reps"
       />
       <DayOneWorkout
         name="#4 Prowler Push"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661377054/Web%20Dev%20Projects/Rectangle-735_p8yt0p.webp"
-        sets="5 sets of 5-12 reps"
+        sets="4-5 sets of 5-12 reps"
       />
       <DayOneWorkout
         name="#5 Barbell Walking Lunge"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661377707/Web%20Dev%20Projects/Rectangle-742-_1__uiiywk.webp"
-        sets="5 sets of 5-12 reps"
+        sets="3-4 sets of 5-12 reps"
       />
       <DayOneWorkout
         name="#6 Barbell Hip Thrust"
         description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
         image="https://res.cloudinary.com/dckwf6med/image/upload/v1661378153/Web%20Dev%20Projects/AnyConv.com__Rectangle_743_1_hjg1ui.webp"
-        sets="5 sets of 5-12 reps"
+        sets="3-4 sets of 5-12 reps"
       />
 
       <Footer />
