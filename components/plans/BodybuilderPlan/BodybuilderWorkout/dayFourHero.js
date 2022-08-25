c3dff55aa1191db41ee24a32d2b85951306093ac
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 02:39:13 +0200

    Create dayFourHero.js

---
+++ components/plans/BodybuilderPlan/BodybuilderWorkout/dayFourHero.js
@@ -0,0 +1,30 @@
+function dayFourHero() {
+    return (
+      <>
+        <div className="sm:flex justify-center sm:h-screen">
+          <div class="relative max-w-8xl">
+            <h2
+              class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-5xl xsm:text-4
+              5xl sm:text-4xl md:lg:text-6xl lg:!text-8xl stroke-removal font-bold text-white top-2/3 md:top-1/2 lg:bottom-0 lg:mr-10 sm:right-1/3 sm:translate-x-2/3 sm:translate-y-1/3"
+            >
+              <span className="stroke-title">REST DAY</span>
+              <br /> DAY 4 OF <br />{" "}
+              <span className="stroke-title">THE PLAN</span>
+            </h2>
+            <img
+              className="bg hidden sm:block h-screen w-screen"
+              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661387066/Web%20Dev%20Projects/AnyConv.com__Group_86_1_fz5b8i.webp"
+            />
+            <img
+              className="bg sm:hidden h-screen w-screen"
+              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661387295/Web%20Dev%20Projects/AnyConv.com__Group_87_1_psdt6l.webp"
+            />
+          </div>
+          {/* <h2 className="text-white mt-7 sm:mt-0 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2> */}
+        </div>
+      </>
+    );
+  }
+  
+  export default dayFourHero;
+  
