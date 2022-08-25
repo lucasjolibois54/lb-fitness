f857b687af4eed5dc2abc3ffc662d40ef80c704a
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 19:47:14 +0200

    Update planOverview.js

--- components/plans/BodybuilderPlan/planOverview.js
+++ components/plans/BodybuilderPlan/planOverview.js
@@ -5,27 +5,28 @@ function planOverview() {
     <>
       <div className="mt-20 mb-20  max-w-7xl  mx-auto flex flex-col sm:flex-row items-center justify-center sm:space-x-14">
         <div className="w-4/6 sm:w-3/12 space-y-4">
-         <a href="/membersonly/bodybuilderPlan"><img
+          
+         <a href="/membersonly/bodybuilderPlan"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
             className="planimg"
             src="https://res.cloudinary.com/dckwf6med/image/upload/v1661359671/Web%20Dev%20Projects/Group_78_1_dlxnfg.webp"
           />
-          <p className="plantitle text-center">Ultimate bodybuilding plan</p></a>
+          <p className="plantitle text-center">Ultimate bodybuilding plan</p></div></a>
         </div>
 
         <div className="w-4/6 sm:w-3/12 space-y-4">
-          <img
+        <div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
             className="planimg"
             src="https://res.cloudinary.com/dckwf6med/image/upload/v1661359795/Web%20Dev%20Projects/Group_79_1_vid11c.webp"
           />
-          <p className="plantitle text-center">Ultimate homeworkout plan</p>{" "}
+          <p className="plantitle text-center">Ultimate homeworkout plan</p>{" "}</div>
         </div>
 
         <div className="w-4/6 sm:w-3/12 space-y-4">
-          <img
+        <div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
             className="planimg"
             src="https://res.cloudinary.com/dckwf6med/image/upload/v1661359970/Web%20Dev%20Projects/Group_79_2_1_bzk1ha.webp"
           />
-          <p className="plantitle text-center">Ultimate HIIT workouts</p>{" "}
+          <p className="plantitle text-center">Ultimate HIIT workouts</p>{" "}</div>
         </div>
       </div>
 
