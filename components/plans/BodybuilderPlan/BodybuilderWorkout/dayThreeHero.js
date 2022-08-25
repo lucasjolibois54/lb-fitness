8ea1fe0fe50af36dacde47c5fcebb1e8c9f39839
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 02:12:09 +0200

    Update dayThreeHero.js

--- components/plans/BodybuilderPlan/BodybuilderWorkout/dayThreeHero.js
+++ components/plans/BodybuilderPlan/BodybuilderWorkout/dayThreeHero.js
@@ -7,17 +7,17 @@ function dayThreeHero() {
               class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-5xl xsm:text-4
               5xl sm:text-4xl md:lg:text-6xl lg:!text-8xl stroke-removal font-bold text-white top-2/3 md:top-1/2 lg:bottom-0 lg:mr-10 sm:right-1/3 sm:translate-x-2/3 sm:translate-y-1/3"
             >
-              <span className="stroke-title">LEG DAY</span>
+              <span className="stroke-title">PULL DAY</span>
               <br /> DAY 3 OF <br />{" "}
               <span className="stroke-title">THE PLAN</span>
             </h2>
             <img
               className="bg hidden sm:block h-screen w-screen"
-              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661379051/Web%20Dev%20Projects/AnyConv.com__Group_82_1_1_yq6qq7.webp"
+              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661385888/Web%20Dev%20Projects/AnyConv.com__Group_84_1_g3uypm.webp"
             />
             <img
               className="bg sm:hidden h-screen w-screen"
-              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661379182/Web%20Dev%20Projects/AnyConv.com__vadim-paripa-MmrSaEJZs4s-unsplash_1_4_1_ua9gj6.webp"
+              src="https://res.cloudinary.com/dckwf6med/image/upload/v1661385975/Web%20Dev%20Projects/AnyConv.com__Group_85_1_uqhbik.webp"
             />
           </div>
           {/* <h2 className="text-white mt-7 sm:mt-0 text-5xl xsm:text-6xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2> */}
