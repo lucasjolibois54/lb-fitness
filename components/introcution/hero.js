5a29096c86233752638a39a85b7c983fb8ad66f2
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-25 18:44:55 +0200

    Update hero.js

--- components/introcution/hero.js
+++ components/introcution/hero.js
@@ -5,7 +5,7 @@ function planHero() {
           <div class="relative max-w-8xl">
             <h2
               class="lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-5xl xsm:text-4
-              5xl sm:text-4xl md:lg:text-6xl lg:!text-8xl stroke-removal font-bold text-white top-2/3 md:top-1/2 lg:bottom-0 lg:mr-10 sm:right-1/3 sm:translate-x-2/3 sm:translate-y-1/3"
+              5xl sm:text-4xl md-lg:text-6xl lg:text-7xl xl:!text-8xl stroke-removal font-bold text-white top-2/3 md:top-1/2 lg:bottom-0 lg:mr-10 sm:right-1/3 sm:translate-x-2/3 sm:translate-y-1/3"
             >
               <span className="stroke-title">READY TO</span>
               <br /> GET STARTED <br />{" "}
