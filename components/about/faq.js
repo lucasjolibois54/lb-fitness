0d42988af65b93582693b109eaaa89dec4641ac1
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 17:32:26 +0200

    Update faq.js

--- components/about/faq.js
+++ components/about/faq.js
@@ -2,9 +2,35 @@ import React from 'react'
 
 function faq() {
   return (
-    <div>
-        
+    <>
+        <img className="absolute -ml-52" src="https://res.cloudinary.com/dckwf6med/image/upload/v1661523827/Web%20Dev%20Projects/Ellipse-100-_1__jahl4k.webp"/>
+
+    <div className='max-w-5xl mx-auto relative z-50'>
+    <h2 className='!leading-tight mb-20 text-white text-center mt-20 sm:mt-20 text-4xl xsm:text-5xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-7xl font-bold stroke-removal'>
+        FAQ's
+    </h2>
+    <div className='border-t border-white mt-5 px-5'>
+        <p className='stroke-title font-bold text-4xl mt-5'>01.</p>
+        <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-4xl font-bold stroke-removal'>HOW MUCH DOES IT COST?</h3>
+        <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-4xl lg:pr-20 text-gray-400 mt-0">
+                LB Fitness is totally free, no hidden fees, and is actually the best free-to-use fitness platform available on the internet </p>
     </div>
+
+    <div className='border-t border-white mt-5 px-5'>
+        <p className='stroke-title font-bold text-4xl mt-5'>02.</p>
+        <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-4xl font-bold stroke-removal'>WHAT CAN I FINT ON THE PLATFORM?</h3>
+        <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-4xl lg:pr-20 text-gray-400 mt-0">
+                The platform will be containing workout &amp; diet plans, fitness related blogs, quizzes to help you achieve your goals, and so much more.               </p>
+    </div>
+    <div className='border-t border-white mt-5 px-5'>
+        <p className='stroke-title font-bold text-4xl mt-5'>03.</p>
+        <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-4xl font-bold stroke-removal'>WHAT SHOULD I DO AFTER SIGN UP?</h3>
+        <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-4xl lg:pr-20 text-gray-400 mt-0">
+                After signing up you will end up on the plan overview page. If you aren't sure how to get started, there is an "intrpduction" button in the navigation, introducing you to the platform and explaining how eveerything works.               </p>
+    </div>
+   
+    </div>
+    </>
   )
 }
 
