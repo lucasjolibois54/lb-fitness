619030a635b1d4dc11649c0c7f8b265af7332088
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 17:35:45 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ import AboutUs from '../components/about/aboutUs';
 import PromoVideo from '../components/about/promoVideo';
 import SubPromoVideo from '../components/about/subPromoVideo';
 import Faq from '../components/about/faq';
+import Footer from '../components/main/footer';
 
 function about() {
   return (
@@ -15,6 +16,7 @@ function about() {
         <PromoVideo/>
         <SubPromoVideo/>
         <Faq/>
+        <Footer/>
     </div>
   )
 }
