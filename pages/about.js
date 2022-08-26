7c3b8bb565f5512b35c25cb3cc8f08c8fce2eb74
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 15:30:39 +0200

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import HomeHeader from '../components/main/homeHeader';
+
+function about() {
+  return (
+    <div className='h-screen'>
+        <HomeHeader/>
+    </div>
+  )
+}
+
+export default about
