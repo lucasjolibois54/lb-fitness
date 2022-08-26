9a0fd709a6d7808dbac4ed291f7400d842a4c43c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 20:24:16 +0200

    Update onInstagram.js

--- components/about/onInstagram.js
+++ components/about/onInstagram.js
@@ -8,7 +8,7 @@ function onInstagram() {
     <div className='e mt-5 px-5'>
         <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-bold stroke-removal'>// ON INSTAGRAM</h3>
         <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-2xl lg:pr-20 text-gray-400 mt-0">
-        LB Fitness is still in the beta release, so if you're interested in following along as well as being updated whenever we add new features. Make sure to give us a follow and join the community.  </p>
+        LB Fitness is still in the beta release, so if you're interested in following along as well as being updated whenever we add new features. Make sure to give us a follow <a href="https://www.instagram.com/lbfitness.eu/" target="_blank" className='text-pink'>@lbfitness.eu</a> and join the community.  </p>
     </div>
 
 
@@ -27,7 +27,7 @@ function onInstagram() {
         <div className="w-4/6 sm:w-4/12 space-y-4">
         <a href="https://www.instagram.com/lbfitness.eu/" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
             className="planimg"
-            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661538003/Web%20Dev%20Projects/Rectangle-745-_2_-_2__otmc4x.webp"
           />
           {/* <p className="plantitle text-center">Ultimate bodybuilding plan</p> */}
           </div></a>
@@ -36,7 +36,7 @@ function onInstagram() {
         <div className="w-4/6 sm:w-4/12 space-y-4">
         <a href="https://www.instagram.com/lbfitness.eu/" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
             className="planimg"
-            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661538104/Web%20Dev%20Projects/Rectangle-745-_3_-_1__qxgmvy.webp"
           />
           {/* <p className="plantitle text-center">Ultimate bodybuilding plan</p> */}
           </div></a>
