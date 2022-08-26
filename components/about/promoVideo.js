c825370d1e97f20011421d4bbeabdf0cffb524c6
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 16:44:02 +0200

    Create promoVideo.js

---
+++ components/about/promoVideo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const promoVideo = () => {
+    return (
+        <>
+            <div className='flex justify-center -mt-10'>
+                <div class="relative max-w-8xl">
+                  
+                <div className="mt-4 top-1/2 absolute lg:top-2/3 left-1/2 -translate-x-1/2">
+                    <a className="text-pink text-2xl py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
+                        Play Video
+                    </a></div>
+                     
+                    <img className='w-full' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661524868/Web%20Dev%20Projects/Group_91_2_cvdd5n.png" />
+                    {/* <h2 class="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
+                <h3 class="absolute text-2xl text-blue-300 top-5 left-5">Top Left</h3>
+                <h3 class="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default promoVideo
+
+
