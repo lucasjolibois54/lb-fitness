1d9cd25ab6932d7ab4eda8d9aff9cd097f085b6a
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 18:35:19 +0200

    Update onInstagram.js

--- components/about/onInstagram.js
+++ components/about/onInstagram.js
@@ -11,6 +11,39 @@ function onInstagram() {
         LB Fitness is still in the beta release, so if you're interested in following along as well as being updated whenever we add new features. Make sure to give us a follow and join the community.  </p>
     </div>
 
+
+
+    <div className="mt-20 mb-20  mx-auto flex flex-col sm:flex-row sm:space-x-14">
+        <div className="w-4/6 sm:w-4/12 space-y-4">
+          
+        <a href="https://www.instagram.com/lbfitness.eu/" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+          />
+          {/* <p className="plantitle text-center">Ultimate bodybuilding plan</p> */}
+          </div></a>
+        </div>
+
+        <div className="w-4/6 sm:w-4/12 space-y-4">
+        <a href="https://www.instagram.com/lbfitness.eu/" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+          />
+          {/* <p className="plantitle text-center">Ultimate bodybuilding plan</p> */}
+          </div></a>
+        </div>
+
+        <div className="w-4/6 sm:w-4/12 space-y-4">
+        <a href="https://www.instagram.com/lbfitness.eu/" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+          />
+          {/* <p className="plantitle text-center">Ultimate bodybuilding plan</p> */}
+          </div></a>
+        </div>
+      </div>
+
+
    
     </div>
     </>
