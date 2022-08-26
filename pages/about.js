e7308fe0b0924793d7b96ca76e27a33cbc70627b
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 15:34:36 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import HomeHeader from '../components/main/homeHeader';
+import Hero from '../components/about/hero';
 
 function about() {
   return (
     <div className='h-screen'>
         <HomeHeader/>
+        <Hero/>
     </div>
   )
 }
