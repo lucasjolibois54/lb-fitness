51077afb7eeade65330bbdf5ceac6f9370afe9fc
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 17:50:30 +0200

    Create onInstagram.js

---
+++ components/about/onInstagram.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function onInstagram() {
+  return (
+    <>
+
+    <div className='max-w-5xl mx-auto mt-48'>
+    <div className='e mt-5 px-5'>
+        <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-bold stroke-removal'>// ON INSTAGRAM</h3>
+        <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-2xl lg:pr-20 text-gray-400 mt-0">
+        LB Fitness is still in the beta release, so if you're interested in following along as well as being updated whenever we add new features. Make sure to give us a follow and join the community.  </p>
+    </div>
+
+   
+    </div>
+    </>
+  )
+}
+
+export default onInstagram
