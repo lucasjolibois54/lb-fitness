6c9c63bb10811bfbc8604a7d0f6b3d1c1672751e
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-26 16:44:06 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,18 @@
 import HomeHeader from '../components/main/homeHeader';
 import Hero from '../components/about/hero';
-
 import TempHero from '../components/about/tempHero';
+import AboutUs from '../components/about/aboutUs';
+import PromoVideo from '../components/about/promoVideo';
+import SubPromoVideo from '../components/about/subPromoVideo';
 
 function about() {
   return (
-    <div className='h-screen'>
+    <div className=''>
         <HomeHeader/>
         <TempHero/>
+        <AboutUs/>
+        <PromoVideo/>
+        <SubPromoVideo/>
     </div>
   )
 }
