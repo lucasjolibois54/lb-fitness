6b4654c90d29b4d9ed29fa9ee998460bee4d0250
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-29 00:14:18 +0200

    Update footer.js

--- components/main/footer.js
+++ components/main/footer.js
@@ -33,7 +33,7 @@ function footer() {
 
       <div className="hidden sm:block w-2/12 -mt-5 space-y-4">
         <p className="mt-6 text-gray-200 font-light ml-3"><a className="hover:text-footer-hover cursor-pointer" href="/about/#faq-section">FAQ'S</a></p>
-        <p className=" text-gray-200 font-light ml-3">Diet Plan</p>
+        <p className=" text-gray-200 font-light ml-3"><a className="hover:text-footer-hover cursor-pointer" href="/membersonly/meals">Meal Plans</a></p>
         <p className=" text-gray-200 font-light ml-3">Find Your Plan</p>
       </div>
     </div>
@@ -59,7 +59,7 @@ function footer() {
 
       <div className=" sm:hidden w-4/12 space-y-4">
         <p className="mt-6 text-gray-200 font-light ml-3"><a className="hover:text-footer-hover cursor-pointer" href="/about/#faq-section">FAQ'S</a></p>
-        <p className=" text-gray-200 font-light ml-3">Diet Plan</p>
+        <p className=" text-gray-200 font-light ml-3"><a className="hover:text-footer-hover cursor-pointer" href="/membersonly/meals">Meal Plans</a></p>
         <p className=" text-gray-200 font-light ml-3">Find Your Plan</p>
       </div>
     </div>
