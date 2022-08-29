88d20543b15641a13d640d9a011ee99d0660fc51
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-29 23:20:07 +0200

    Update hero.js

--- components/blogs/hero.js
+++ components/blogs/hero.js
@@ -4,13 +4,13 @@ function hero() {
   return (
     <>
     <div className='max-w-7xl mx-auto'>
-        <h1 className='!leading-tight text-white text-left px-5 mt-7 sm:mt-36 text-5xl xsm:text-5xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold stroke-removal'>
+        <h1 className='!leading-tight text-white text-left px-5 mt-28 sm:mt-36 text-5xl xsm:text-3xl sm:text-4xl md:6xl md-lg:text-4xl lg:text-5xl font-bold stroke-removal'>
         READ OUR FITNESS, NUTRITION <br/>
 AND LIFESTYLE BLOGS
         </h1>
         <img className='mt-7 px-5' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661807512/Web%20Dev%20Projects/image_60_1_vx4bfn.webp'/>
         <p className='mt-5 px-5 sm:w-2/3 font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-        <hr className='mt-16 px-5'/>
+        <hr className='mt-7 sm:mt-16 px-5'/>
     </div>
     </>
   )
