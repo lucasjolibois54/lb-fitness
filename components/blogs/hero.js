7ae93e82d4285ed34390526d7f18edf2899115cc
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-29 23:13:04 +0200

    Update hero.js

--- components/blogs/hero.js
+++ components/blogs/hero.js
@@ -3,8 +3,12 @@ import React from 'react'
 function hero() {
   return (
     <>
-    <div>
-        
+    <div className='max-w-7xl mx-auto'>
+        <h1 className='!leading-tight text-white text-left px-5 mt-7 sm:mt-36 text-5xl xsm:text-5xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold stroke-removal'>
+        READ OUR FITNESS, NUTRITION <br/>
+AND LIFESTYLE BLOGS
+        </h1>
+        <img className='mt-7 px-5' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661807512/Web%20Dev%20Projects/image_60_1_vx4bfn.webp'/>
     </div>
     </>
   )
