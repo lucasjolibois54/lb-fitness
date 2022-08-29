5adf565d22e2e9a795ae9defa60afd950eafc473
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-29 23:22:19 +0200

    Update blogs.js

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderAfterLogin from '../components/main/headerAfterLogin'
 import Hero from '../components/blogs/hero'
+import FeaturedBlogs from '../components/blogs/featuredBlogs'
 
 function blogs() {
   return (
     <div className='h-screen'>
       <HeaderAfterLogin/>
       <Hero/>
+      <FeaturedBlogs/>
     </div>
   )
 }
