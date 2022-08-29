7097fca6b4a23f47ac0e0c8ac2f0ebef49088cbb
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-29 23:27:12 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function featuredBlogs() {
   return (
-    <div>featuredBlogs</div>
+    <div className='my-16 max-w-7xl mx-auto px-6'>
+        <h3 className='!leading-tight text-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
+            FEATURED BLOGS
+        </h3>
+    </div>
   )
 }
 
