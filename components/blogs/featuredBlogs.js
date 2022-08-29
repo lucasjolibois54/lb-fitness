9b27663bab29122a454ac92ddaa5b7f4a39f0d3a
lucasjolibois54 <lucasjolibois@icloud.com>
2022-08-30 00:08:43 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -9,14 +9,12 @@ function featuredBlogs() {
 
 
 
-        <div className='mt-12 flex space-x-8'>
-            <img className='w-6/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
-            <img className='9/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808920/Web%20Dev%20Projects/image_62_1_tbz8z1.webp'/>
+        <div className='mt-12 w-screen flex flex-row space-x-8'>
+            <img className='w-3/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
+            <img className='w-7/12 pr-6' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808920/Web%20Dev%20Projects/image_62_1_tbz8z1.webp'/>
         </div>
-        <div className='mt-10 flex space-x-8'>
-        <img className='9/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808920/Web%20Dev%20Projects/image_62_1_tbz8z1.webp'/>
-            <img className='w-6/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
-        </div>
+       
+       
         <div className="mt-10 pt-5 flex justify-center">
           <a href="#" className="text-center text-pink py-3 px-12 rounded-full border-gray-stroke border-2 cursor-pointer">
             View All
