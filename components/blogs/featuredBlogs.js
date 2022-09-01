bb3caed869c01d67f0a8986d12d8e25c3b3bd647
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-01 18:17:03 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -3,23 +3,25 @@ import React from 'react'
 function featuredBlogs() {
   return (
     <div className='my-16 max-w-7xl mx-auto px-6'>
-        <h3 className='!leading-tight text-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
+ <h3 className='!leading-tight mb-16 text-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
             FEATURED BLOGS
         </h3>
 
 
+<div className="">
+  <div className="grid grid-cols-3 gap-2 grid-flow-row grid-">
+    <div className="p-5 text-center bg-transparent col-start-1 col-end-2">
+    <img className='w-full' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
+    </div>
 
-        <div className='mt-12 w-screen flex flex-row space-x-8'>
-            <img className='w-3/12' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
-            <img className='w-7/12 pr-6' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808920/Web%20Dev%20Projects/image_62_1_tbz8z1.webp'/>
-        </div>
-       
-       
-        <div className="mt-10 pt-5 flex justify-center">
-          <a href="/allBlogs" className="text-center text-pink py-3 px-12 rounded-full border-gray-stroke border-2 cursor-pointer">
-            View All
-          </a>
-        </div>
+    <div className="p-5 text-center bg-transparent col-start-2 col-end-4">
+    <img className='w-full pr-6' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808920/Web%20Dev%20Projects/image_62_1_tbz8z1.webp'/>
+    </div>
+    {/* <div className="p-5 text-center bg-white">C</div>
+    <div className="p-5 text-center bg-white">D</div>
+    <div className="p-5 text-center bg-white">E</div> */}
+  </div>
+</div>
     </div>
   )
 }
