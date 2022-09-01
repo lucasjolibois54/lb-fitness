754fa9ef3032ee3cae5fc31266ea0c1dc0951637
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-01 18:19:49 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -3,12 +3,12 @@ import React from 'react'
 function featuredBlogs() {
   return (
     <div className='my-16 max-w-7xl mx-auto px-6'>
- <h3 className='!leading-tight mb-16 text-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
+ <h3 className='!leading-tight mb-10 text-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
             FEATURED BLOGS
         </h3>
 
 
-<div className="">
+<div className="mx-auto">
   <div className="grid grid-cols-3 gap-2 grid-flow-row grid-">
     <div className="p-5 text-center bg-transparent col-start-1 col-end-2">
     <img className='w-full' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
