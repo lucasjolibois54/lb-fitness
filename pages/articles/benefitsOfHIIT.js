617d023e33ebea24c2f276b4349511726866b171
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:08:21 +0200

    Create benefitsOfHIIT.js

---
+++ pages/articles/benefitsOfHIIT.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../../components/blogs/blog-articles/hero'
+
+function benefitsOfHIIT() {
+  return (
+    <div>
+
+        <Hero/>
+        
+        benefitsOfHIIT</div>
+  )
+}
+
+export default benefitsOfHIIT
