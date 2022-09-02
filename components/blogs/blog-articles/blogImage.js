c8d08918920eb670768a3216009dfd74220dc686
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 21:12:13 +0200

    Update blogImage.js

--- components/blogs/blog-articles/blogImage.js
+++ components/blogs/blog-articles/blogImage.js
@@ -4,7 +4,7 @@ function blogImage(props) {
   return (
     <>
     <div className='max-w-3xl mx-auto'>
-    <div className='ml-0 mr-7'>
+    <div className='sm:pl-12 md-blog-img:pl-7 md-blog-img:pr-7 lg-blog-img:pl-4 lg-blog-img:pr-4 md-lg-bigger:pl-0 md-lg-bigger:pr-0 sm:mr-12'>
                     <img className='w-screen p-7 sm:p-0 sm:mt-7' src={props.image}/>
     </div></div>
     </>
