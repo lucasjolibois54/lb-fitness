700ab98238da6bac783d2aaac328ccaf5697da13
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:12:47 +0200

    Update benefitsOfHIIT.js

--- pages/articles/benefitsOfHIIT.js
+++ pages/articles/benefitsOfHIIT.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../../components/blogs/blog-articles/hero'
+import HomeHeader from '../../components/main/homeHeader'
 
 function benefitsOfHIIT() {
   return (
     <div>
-
+      <HomeHeader/>
         <Hero/>
         
         benefitsOfHIIT</div>
