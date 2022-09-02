6eebf01769039e8eeb2c0a7fb9ad9d8bd8411ce2
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:50:10 +0200

    Update textSlider.js

--- components/blogs/blog-articles/textSlider.js
+++ components/blogs/blog-articles/textSlider.js
@@ -3,7 +3,7 @@ import React from 'react'
 function textSlider() {
   return (
     <div>
-                    <div className="mt-20 pb-20">
+                    <div className="mt-20 pb-12">
 
 <div className="whitespace-nowrap">
     <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">HIIT WORKOUT EXAMPLES HIIT WORKOUT EXAMPLES HIIT WORKOUT EXAMPLE
