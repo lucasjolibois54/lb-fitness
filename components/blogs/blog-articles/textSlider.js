f532f5123d5904b0821b3ad7448f51f996e18ec5
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:26:50 +0200

    Update textSlider.js

--- components/blogs/blog-articles/textSlider.js
+++ components/blogs/blog-articles/textSlider.js
@@ -6,7 +6,7 @@ function textSlider() {
                     <div className="mt-20 pb-20">
 
 <div className="whitespace-nowrap">
-    <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-7xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
+    <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">HIIT WORKOUT EXAMPLES HIIT WORKOUT EXAMPLES HIIT WORKOUT EXAMPLE
     </p>
 </div>
 
