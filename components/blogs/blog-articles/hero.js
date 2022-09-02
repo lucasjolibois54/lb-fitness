ca23e0735e3cdeb809fe0b8efde7e1a973f04582
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:28:02 +0200

    Update hero.js

--- components/blogs/blog-articles/hero.js
+++ components/blogs/blog-articles/hero.js
@@ -5,8 +5,10 @@ function hero() {
     <>
     <div className='mt-36 max-w-7xl mx-auto'>
     <a href='/blogs'><img className='w-16' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662136014/Web%20Dev%20Projects/Frame_ogtaop.svg'/></a>
-    <h1>Benefits of High Intensity Interval Training (HIIT)</h1>
-    </div>
+    <h1 className='sm:ml-7 max-w-4xl !leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-bold stroke-removal'>Benefits of High Intensity Interval Training (HIIT)</h1>
+    <p className='sm:ml-7 mt-3 text-gray-subtext font-extralight'>READ TIME • 3 MIN</p>    </div>
+    <img className='w-screen mt-16' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662139445/Web%20Dev%20Projects/image_60_1_1_jwn4un.webp'/>
+
     </>
   )
 }
