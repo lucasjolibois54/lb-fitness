129c2286fb9fa1be93d5a18a2ad1835bc15402dc
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:17:21 +0200

    Update hero.js

--- components/blogs/blog-articles/hero.js
+++ components/blogs/blog-articles/hero.js
@@ -3,8 +3,9 @@ import React from 'react'
 function hero() {
   return (
     <>
-    <div className='mt-36 max-w-7xl mx-auto'>hero</div>
-
+    <div className='mt-36 max-w-7xl mx-auto'>
+    <a href='/blogs'><img className='w-16' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662136014/Web%20Dev%20Projects/Frame_ogtaop.svg'/></a>
+    hero</div>
     </>
   )
 }
