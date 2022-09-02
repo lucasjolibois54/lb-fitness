db44494b83beffdf982befb1cb5da6d19cf463f1
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:39:56 +0200

    Update hero.js

--- components/blogs/blog-articles/hero.js
+++ components/blogs/blog-articles/hero.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-function hero() {
+function hero(props) {
   return (
     <>
     <div className='mt-36 max-w-7xl mx-auto'>
     <a href='/blogs'><img className='w-16 ml-3' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662136014/Web%20Dev%20Projects/Frame_ogtaop.svg'/></a>
-    <h1 className='sm:ml-7 max-w-4xl text-center sm:text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-bold '>Benefits of High Intensity Interval Training (HIIT)</h1>
-    <p className='sm:ml-7 mt-3 text-center sm:text-left text-gray-subtext font-extralight'>READ TIME • 3 MIN</p>    </div>
-    <img className='w-screen mt-16' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662139445/Web%20Dev%20Projects/image_60_1_1_jwn4un.webp'/>
-
+    <h1 className='sm:ml-7 max-w-4xl text-center sm:text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-bold '>{props.title}</h1>
+    <p className='sm:ml-7 mt-3 text-center sm:text-left text-gray-subtext font-extralight'>{props.readTime}</p>    </div>
+    <img className='w-screen mt-16' src={props.image}/>
     </>
   )
 }
