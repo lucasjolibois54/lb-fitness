9ccf81da387096308c5840d1e0bc226d0d6a00ec
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:17:02 +0200

    Update textSection.js

--- components/blogs/blog-articles/textSection.js
+++ components/blogs/blog-articles/textSection.js
@@ -4,8 +4,8 @@ function textSection(props) {
   return (
     <div>
             <div className='mt-20 3xl:mt-28 max-w-4xl mx-auto'>
-    <h1 className='sm:ml-7 max-w-4xl text-center sm:text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold '>{props.title}</h1>
-    <p className='sm:ml-7 max-w-3xl mt-3 text-center sm:text-left text-gray-subtext font-extralight'>{props.paragraph}</p>    </div>
+    <h1 className='sm:ml-7 max-w-4xl text-center sm:text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-medium '>{props.title}</h1>
+    <p className='sm:ml-7 sm:mt-8 max-w-3xl mt-3 text-center sm:text-left text-gray-subtext font-extralight'>{props.paragraph}</p>    </div>
     </div>
   )
 }
