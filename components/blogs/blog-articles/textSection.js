3c3d3a009b80f94ad45c36ba0738d028a896ef96
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:40:06 +0200

    Update textSection.js

--- components/blogs/blog-articles/textSection.js
+++ components/blogs/blog-articles/textSection.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function textSection(props) {
   return (
-    <div>
-            <div className='mt-20 3xl:mt-28 max-w-4xl !mx-auto mr-7 ml-7'>
+    <>
+    <div className='max-w-4xl mx-auto'>
+            <div className='mt-20 3xl:mt-28 mr-7 ml-7'>
     <h1 className='sm:ml-7 max-w-4xl text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-medium '>{props.title}</h1>
     <p className='sm:ml-7 sm:mt-8 max-w-3xl mt-6 text-left text-gray-subtext font-extralight'>{props.paragraph}</p>    </div>
     </div>
+    </>
   )
 }
 
