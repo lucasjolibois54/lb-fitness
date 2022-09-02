38f981552066cc12e04f7d1fa6be35d7c963149c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:17:00 +0200

    Create textSlider.js

---
+++ components/blogs/blog-articles/textSlider.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function textSlider() {
+  return (
+    <div>
+                    <div className="mt-20 pb-20">
+
+<div className="whitespace-nowrap">
+    <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-7xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
+    </p>
+</div>
+
+</div>
+    </div>
+  )
+}
+
+export default textSlider
