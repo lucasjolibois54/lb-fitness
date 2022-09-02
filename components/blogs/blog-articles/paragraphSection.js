49f0528e03ddff7a57b00b5ffcb39f905af94b3c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 20:26:43 +0200

    Update paragraphSection.js

--- components/blogs/blog-articles/paragraphSection.js
+++ components/blogs/blog-articles/paragraphSection.js
@@ -3,8 +3,8 @@ import React from 'react'
 function paragraphSection(props) {
   return (
     <div>
-            <div className='mt-6 3xl:mt-28 max-w-4xl mx-auto'>
-    <p className='sm:ml-7 max-w-3xl mt-3 text-center sm:text-left text-gray-subtext font-extralight'>{props.paragraph}</p>    </div>
+            <div className='mt-6 3xl:mt-28 max-w-4xl mx-auto mr-7 ml-7'>
+    <p className='sm:ml-7 max-w-3xl mt-3 text-left text-gray-subtext font-extralight'>{props.paragraph}</p>    </div>
     </div>
   )
 }
