678b5f588b20b14285d2ff2e52a2a02b80f31c6d
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-02 19:08:24 +0200

    Update hero.js

--- components/blogs/hero.js
+++ components/blogs/hero.js
@@ -8,7 +8,7 @@ function hero() {
         READ OUR FITNESS, NUTRITION <br/>
 AND LIFESTYLE BLOGS
         </h1>
-        <img className='mt-7 px-5' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661807512/Web%20Dev%20Projects/image_60_1_vx4bfn.webp'/>
+        <a href='/articles/benifitsOfHIIT'><img className='mt-7 px-5' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661807512/Web%20Dev%20Projects/image_60_1_vx4bfn.webp'/></a>
         <p className='mt-5 px-5 sm:w-2/3 font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
         <hr className='mt-7 sm:mt-9 px-5'/>
     </div>
