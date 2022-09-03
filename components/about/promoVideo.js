c4eccaa64f9a8ca2c55e2d92cf804fa097842a72
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 16:19:12 +0200

    Update promoVideo.js

--- components/about/promoVideo.js
+++ components/about/promoVideo.js
@@ -5,7 +5,7 @@ const promoVideo = () => {
     return (
         <>
             <div id='the-video' className='flex justify-center mt-20 sm:-mt-10'>
-                <div class="relative max-w-8xl">
+                <div className="relative max-w-8xl">
                   
                 <div className="mt-4 top-1/2 absolute lg:top-2/3 left-1/2 -translate-x-1/2">
                     <a  target="blank" href='https://res.cloudinary.com/dckwf6med/video/upload/v1661535482/Web%20Dev%20Projects/Workout_video_kibdhs.mp4' className="text-pink text-2xl py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
@@ -21,9 +21,9 @@ const promoVideo = () => {
           Your browser does not support the video tag.
         </video> */}
 
-                    {/* <h2 class="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
-                <h3 class="absolute text-2xl text-blue-300 top-5 left-5">Top Left</h3>
-                <h3 class="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
+                    {/* <h2 className="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
+                <h3 className="absolute text-2xl text-blue-300 top-5 left-5">Top Left</h3>
+                <h3 className="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
                 </div>
             </div>
         </>
