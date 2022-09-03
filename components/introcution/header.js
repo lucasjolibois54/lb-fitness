163c87d39309d157f9392fa280df1a29343e4391
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 18:59:21 +0200

    Update header.js

--- components/introcution/header.js
+++ components/introcution/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
 function header() {
   return (
     <div className=' zzz max-w-7xl mx-auto font-bold'>
     <div className="flex flex-col sm:block">
-    <p className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><a href="/">LB│FITNESS</a></p>
+    <p className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><Link href="/">LB│FITNESS</Link></p>
     <div className="flex flex-row sm:block">
-    <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</a>
-    <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/">Workouts</a>
+    <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</Link>
+    <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/">Workouts</Link>
     </div> </div></div>
   )
 }
