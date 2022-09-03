e3937cdec23bf862b48fca5918ece32e375d9e7e
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 17:17:19 +0200

    Update blogs.js

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeaderAfterLogin from '../components/main/headerAfterLogin'
+import HomeHeader from '../components/main/homeHeader';
 import Hero from '../components/blogs/hero'
 import FeaturedBlogs from '../components/blogs/featuredBlogs'
 import Footer from '../components/main/footer';
@@ -7,7 +7,7 @@ import Footer from '../components/main/footer';
 function blogs() {
   return (
     <div className='h-screen'>
-      <HeaderAfterLogin/>
+      <HomeHeader/>
       <Hero/>
       <FeaturedBlogs/>
       <Footer/>
