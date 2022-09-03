b3387a26c93af9954bb1ab6f8492e7ff9e7dc887
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 15:30:45 +0200

    Update boostYourTestosteroneNaturally.js

--- pages/articles/boostYourTestosteroneNaturally.js
+++ pages/articles/boostYourTestosteroneNaturally.js
@@ -58,11 +58,11 @@ function boostYourTestosteroneNaturally() {
 
       <TextSection
         title="Summary"
-        paragraph="As mentioned throughout the blog article, there are a lot of benefits when doing high intensity interval training, additionally, there are also a lot of different ways (exercises) to practice HIIT. It's just important to keep your heart rate up, take breaks (active rest) in between your “sets”. When doing the actual exercises, go as hard as you can. Here, we just went through some of them, as the list can go on forever. If you would like to see more HIIT exercises, click on the link below in order to access our full HIIT workout plan."
+        paragraph="As mentioned throughout the blog article, side effects from having a low testosterone level, including things like less muscle mass, weight gain, thinner bones, decreased sex drive, and so much more. Fortunately there are alot of things you can do, to boost your testosterone naturally, such as lifting heavy weights, having a balanced diet and not to drink excessively. If you would like to find the right diet for you, click the link below."
         />
         <ExternalLinking
-        text="See the HIIT workout plan"
-        link="/membersonly/workoutPlans"/>
+        text="See all our meal plans"
+        link="/membersonly/meals"/>
 
         <Footer/></div>
   )
