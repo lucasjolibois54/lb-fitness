0c8d9ff650025ebcb844edae2de289c8727308d9
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 16:19:16 +0200

    Update hero.js

--- components/home/hero.js
+++ components/home/hero.js
@@ -5,13 +5,13 @@ function hero() {
         <div className="p-0 pt-10 flex flex-col w-full lg:flex-row mb-4">
 
             {/* Max width container, center aligned, with some padding */}
-            <div class="max-w-9xl mx-auto lg:px-0 sm:px-6 py-8">
+            <div className="max-w-9xl mx-auto lg:px-0 sm:px-6 py-8">
 
                 {/* <!-- Grid columns + some font styles for the children elements to inherit --> */}
-                <div class="font-medium leading-7 space-y-2 sm:grid sm:grid-cols-2 lg:grid-cols-2 sm:gap-4 sm:space-y-0">
+                <div className="font-medium leading-7 space-y-2 sm:grid sm:grid-cols-2 lg:grid-cols-2 sm:gap-4 sm:space-y-0">
 
                     {/* <!-- Grid cell #1 --> */}
-                    <div class=" py-3 px-6 md-lg:ml-5 xl:ml-20 md-lg:mt-10 xl:mt-20 rounded">
+                    <div className=" py-3 px-6 md-lg:ml-5 xl:ml-20 md-lg:mt-10 xl:mt-20 rounded">
 
                         <h1 className="text-white mt-7 sm:mt-0 text-6xl xsm:text-7xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FITNESS <span className="stroke-removal"><br />TRAINING </span><br />PLAN</h1>
                         <p className="text-base md:text-lg text-dark-text py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
@@ -24,7 +24,7 @@ function hero() {
 
 
                     {/* <!-- Grid cell #2 --> */}
-                    <div class="py-3 float-right rounded">
+                    <div className="py-3 float-right rounded">
                         <img
                             className="w-full  xl:w-6/6 float-right"
                             src={
