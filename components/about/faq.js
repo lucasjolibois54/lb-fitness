df80c4a69c7dcc397f27e90ca75aad29634f4d2a
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 18:11:31 +0200

    Update faq.js

--- components/about/faq.js
+++ components/about/faq.js
@@ -7,7 +7,7 @@ function faq() {
 
     <div id='faq-section' className='max-w-5xl mx-auto relative z-50'>
     <h2 className='!leading-tight mb-20 text-white text-center mt-20 sm:mt-20 text-4xl xsm:text-5xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-7xl font-bold stroke-removal'>
-        FAQ's
+        FAQ&apos;s
     </h2>
     <div className='border-t border-white mt-5 px-5'>
         <p className='stroke-title font-bold text-4xl mt-5'>01.</p>
@@ -26,7 +26,7 @@ function faq() {
         <p className='stroke-title font-bold text-4xl mt-5'>03.</p>
         <h3 className='leading-tight mt-4 text-white text-4xl xsm:text-3xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-4xl font-bold stroke-removal'>WHAT SHOULD I DO AFTER SIGN UP?</h3>
         <p className="text-base font-light md:text-lg py-5 lg:px-24 lg:pl-0 max-w-4xl lg:pr-20 text-gray-400 mt-0">
-                After signing up you will end up on the plan overview page. If you aren't sure how to get started, there is an "introduction" button in the navigation, introducing you to the platform and explaining how eveerything works.               </p>
+                After signing up you will end up on the plan overview page. If you aren&apos;t sure how to get started, there is an 	&quot;introduction&quot; button in the navigation, introducing you to the platform and explaining how eveerything works.               </p>
     </div>
    
     </div>
