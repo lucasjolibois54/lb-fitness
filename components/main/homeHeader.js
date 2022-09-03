c78ee83f12f8d905b8fc40762a6dc3e6dd2bc308
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 20:13:51 +0200

    Update homeHeader.js

--- components/main/homeHeader.js
+++ components/main/homeHeader.js
@@ -61,7 +61,12 @@ function homeHeader() {
    
     return (                   
       <>
-<h2>nom</h2>
+    <div className=' zzz max-w-7xl mx-auto font-bold'>
+    <div className="flex flex-col sm:block">
+    <p href="/" className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><Link href="/">LB│FITNESS</Link></p>
+    <div className="flex flex-row sm:block">
+    <p className='float-right mx-7 ml-0 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/login">Login</Link></p>
+    </div> </div></div>
       
       </>
     );
