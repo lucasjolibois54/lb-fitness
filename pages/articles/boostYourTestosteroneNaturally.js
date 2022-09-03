d9496b1f74096689d33c5e1d3c2c09dbe7b9ef5b
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 15:11:09 +0200

    Create boostYourTestosteroneNaturally.js

---
+++ pages/articles/boostYourTestosteroneNaturally.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Hero from '../../components/blogs/blog-articles/hero'
+import HomeHeader from '../../components/main/homeHeader'
+import TextSection from '../../components/blogs/blog-articles/textSection'
+import ParagraphSection from '../../components/blogs/blog-articles/paragraphSection'
+import TextSlider from '../../components/blogs/blog-articles/textSlider'
+import SubTextSection from '../../components/blogs/blog-articles/subTextSection'
+import BlogImage from '../../components/blogs/blog-articles/blogImage'
+import ExternalLinking from '../../components/blogs/blog-articles/externalLinking'
+import Footer from '../../components/main/footer'
+
+function boostYourTestosteroneNaturally() {
+  return (
+    <div>
+      <HomeHeader/>
+        <Hero
+        title="How to Boost Your Testosterone Naturally"
+        readTime="READ TIME • 3 MIN"
+        image="https://res.cloudinary.com/dckwf6med/image/upload/v1662207131/Web%20Dev%20Projects/Mask_group_6_1_zbjfue.webp"
+        />
+        
+        <TextSection
+        title="What is Testosterone?"
+        paragraph="Testosterone is a hormone which is found in both men and women. For men, the testicles primarily make testosterone, while women’s ovaries also makes testosterone, but in much smaller amounts. Testosterone is oftenly associated sex drive and sperm quality, but also affects your bones, muscle mass, fat storage in the human body, and even red blood cell production."
+        />
+
+        <ParagraphSection
+                paragraph="Unfortunately, the production of testosterone usually starts to increase significantly during puberty but begins to dip after the age 30. Low levels of testosterone, can produce a variety of symptoms in, including: weight gain, thinner bones, decreased sex drive, moodiness, less energylow, self-esteem, less body hair, less muscle mass and so much more. Having a high Testosterone is key for building muscle mass which is why we will now go over how to naturally increase your T-levels."
+/>
+
+        <TextSlider
+        text="HOW TO BOOST TESTOSTERONE HOW TO BOOST TESTOSTERONE HOW TO BOOST TESTOSTERONE "/>
+
+        <SubTextSection
+         title="1.- Hit the weights"
+         paragraph="Research shows that lifting heavier weights is the best way of exercise to boost your testosterone naturally. As you begin to increase you muscle mass, it will trigger the body to produce more testosterone. You focus on your bigger muscle groups, like the chest, back, quadriceps and hamstrings. This doesn’t mean you should ignore the smaller muscle groups though. Working on smaller muscle groups as well, will strenghen your entire body whcih will help prevent injuries and speeds up your metabolism."
+         />
+         <BlogImage
+                 image="https://res.cloudinary.com/dckwf6med/image/upload/v1662153402/Web%20Dev%20Projects/Mask_group_2_2_ajp5hg.webp"
+                 />
+
+<SubTextSection
+         title="2.- Maintain a balanced diet"
+         paragraph="Research has shown that eating well is key to maintaining testosterone levels, as well as increasing them. Low testosterone levels and being overweight may contribute to a variety of inflammatory conditions and impaired neurological function. The best is to include whole foods and meat which offers a good balance of proteins, carbs and fats. Eating healthy and nutritious diet can keep all hormones levels balanced and promote optimal long-term health and increased testosterone."
+         />
+          <BlogImage
+                 image="https://res.cloudinary.com/dckwf6med/image/upload/v1662153757/Web%20Dev%20Projects/Mask_group_3_1_lt7t4s.webp"
+                 />
+
+<SubTextSection
+         title="3.- Be aware with alcohol consumption"
+         paragraph="Drinking too much alcohol eventually decreases blood testosterone in men due an internal effect on the testicular level. Drinking alcohol also results a release of certain endorphins. While these endorphins might feel pleasant and relaxing, they can also interfere with the testosterone synthesis. Generally speaking, one or two drinks occasionally shouldn’t impact the testosterone levels too much, just remember too to drink excessively."
+         />
+          <BlogImage
+                 image="https://res.cloudinary.com/dckwf6med/image/upload/v1662153972/Web%20Dev%20Projects/Mask_group_4_1_wfmzgl.webp"
+                 />
+      
+
+      <TextSection
+        title="Summary"
+        paragraph="As mentioned throughout the blog article, there are a lot of benefits when doing high intensity interval training, additionally, there are also a lot of different ways (exercises) to practice HIIT. It's just important to keep your heart rate up, take breaks (active rest) in between your “sets”. When doing the actual exercises, go as hard as you can. Here, we just went through some of them, as the list can go on forever. If you would like to see more HIIT exercises, click on the link below in order to access our full HIIT workout plan."
+        />
+        <ExternalLinking
+        text="See the HIIT workout plan"
+        link="/membersonly/workoutPlans"/>
+
+        <Footer/></div>
+  )
+}
+
+export default boostYourTestosteroneNaturally
