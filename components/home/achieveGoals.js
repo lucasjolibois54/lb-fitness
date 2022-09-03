9cc88b2e4f6bb6928672d24be2e1e8416f37d3cb
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 23:33:39 +0200

    Update achieveGoals.js

--- components/home/achieveGoals.js
+++ components/home/achieveGoals.js
@@ -5,11 +5,16 @@ const achieveGoals = () => {
         <>
             <div className='flex justify-center'>
                 <div className="relative max-w-8xl">
-                    <h2 className="border-none lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
+                    <h2 className="hidden sm:block border-none lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
                         Achieve your training <br />
                         goals with the right plan, <br />
                         not with luck.</h2>
-                    <img alt="Image" className='w-full' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png" />
+                        <h2 className=" sm:hidden border-none lg:-mt-36 lg:-ml-0 !leading-tight absolute ml-3 sm:ml-10 text-4xl sm:text-3xl sm-lg:text-4xl lg:text-5xl font-bold text-white top-1/2 lg:top-2/3 left-1/3 -translate-x-1/2 -translate-y-1/2 text-transparent bg-clip-text bg-gradient-to-r from-off-white to-gradient-off-orange">
+                        Achieve your training
+                        goals with the right plan,
+                        not with luck.</h2>
+                    <img alt="Image" className='w-full hidden sm:block' src="https://res.cloudinary.com/dckwf6med/image/upload/v1661271589/Web%20Dev%20Projects/achieve_ropfbh.png" />
+                    <img alt="Image" className='w-full sm:hidden' src="https://res.cloudinary.com/dckwf6med/image/upload/v1662240584/Web%20Dev%20Projects/Group_69_1_1_dqcjqh.webp" />
                     {/* <h2 className="absolute text-3xl text-amber-400 bottom-4 left-1/2 -translate-x-1/2">Bottom Center</h2>
                 <h3 className="absolute text-2xl text-blue-300 top-5 left-5">Top Left</h3>
                 <h3 className="absolute text-2xl text-green-300 bottom-5 right-5">Bottom Right</h3> */}
