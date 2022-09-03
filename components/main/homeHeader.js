956d37629856bd1e575190a07e72fc6abf700b57
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 18:59:08 +0200

    Update homeHeader.js

--- components/main/homeHeader.js
+++ components/main/homeHeader.js
@@ -1,5 +1,6 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link'
 
 const HeaderAuth = () => { 
   return (
@@ -7,7 +8,7 @@ const HeaderAuth = () => {
     <div className="flex flex-col sm:block">
     <p href="/" className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><a href="/">LB│FITNESS</a></p>
     <div className="flex flex-row sm:block">
-    <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</a>
+    <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</Link>
     </div> </div></div>
   )
 }
@@ -19,7 +20,7 @@ const Header = () => {      // variable with the component Header. () => means i
     <div className="flex flex-col sm:block">
     <p href="/" className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><a href="/">LB│FITNESS</a></p>
     <div className="flex flex-row sm:block">
-    <a className='float-right mx-7 ml-0 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/login">Login</a>
+    <Link className='float-right mx-7 ml-0 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/login">Login</Link>
     {/* <a className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/about">About</a> */}
     </div> </div></div>
   )
