f33b314ec470149009b946f17cdeab2cd0125395
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 20:11:53 +0200

    Update homeHeader.js

--- components/main/homeHeader.js
+++ components/main/homeHeader.js
@@ -1,7 +1,7 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { useUser } from '@auth0/nextjs-auth0';
 import Link from 'next/link'
-
+/*
 const HeaderAuth = () => { 
   return (
     <div className=' zzz max-w-7xl mx-auto font-bold'>
@@ -21,16 +21,15 @@ const Header = () => {      // variable with the component Header. () => means i
     <p href="/" className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><Link href="/">LB│FITNESS</Link></p>
     <div className="flex flex-row sm:block">
     <p className='float-right mx-7 ml-0 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/login">Login</Link></p>
-    {/* <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/about">About</Link> */}
     </div> </div></div>
   )
 }
-
+*//*
 
 function homeHeader() {
-  const { user, error, isLoading } = useUser();
+  const { user, error, isLoading } = useUser();           */
   /*console.log(user)*/
-
+/*
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
@@ -42,6 +41,27 @@ function homeHeader() {
       ) : (                     // : is the same as else
       <Header/>
       )}
+      
+      </>
+    );
+
+}
+
+export default homeHeader*/
+
+
+
+
+function homeHeader() {
+       
+  /*console.log(user)*/
+
+
+
+   
+    return (                   
+      <>
+<h2>nom</h2>
       
       </>
     );
