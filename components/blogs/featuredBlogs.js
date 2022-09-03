1d8f9c70e4e6db8b238469e66031e393e7123550
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 00:04:20 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -20,7 +20,7 @@ function featuredBlogs() {
     <div className="p-5 sm:p-2 text-center bg-transparent  sm:col-start-2 sm:col-end-4">
     <Link href='/articles/boostYourTestosteroneNaturally'><img alt="Image" className='w-full cursor-pointer hidden sm:block' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662213064/Web%20Dev%20Projects/Mask_group_7_1_da5tie.webp'/></Link>
     <Link href='/articles/boostYourTestosteroneNaturally'><img alt="Image" className='w-full cursor-pointer sm:hidden' src='https://res.cloudinary.com/dckwf6med/image/upload/v1662213126/Web%20Dev%20Projects/Mask_group_8_1_e0c1mp.webp'/></Link>
-    <p className='mt-5 text-left text-gray-subtext text-sm px-2 font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+    <p className='mt-5 text-left text-gray-subtext text-sm px-2 font-extralight'>Testosterone is oftenly associated sex drive and sperm quality, but also affects your bones, muscle mass, fat storage in the human body, and even red blood cell production...</p>
     <Link href='/articles/boostYourTestosteroneNaturally'><p className='cursor-pointer mt-2 text-left text-pink text-sm px-2 font-extralight'>Read more</p></Link>
     </div>
   </div>
