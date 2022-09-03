e3e4084b94c52218fcd4350ef82175019a52c1b0
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-03 19:21:38 +0200

    Update headerAfterLogin.js

--- components/main/headerAfterLogin.js
+++ components/main/headerAfterLogin.js
@@ -6,8 +6,8 @@ function headerAfterLogin() {
         <div className="flex flex-col sm:block">
         <p href="/" className='text-center float-left text-2xl pt-2 sm:pt-0 sm:text-3xl mx-7 -mb-7 mt-5 sm:mt-12 pb-4 sm:pb-0 border-white/20 sm:border-none border-b'><Link href="/">LB│FITNESS</Link></p>
         <div className="flex flex-row sm:block">
-        <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/api/auth/logout">Logout</Link>
-        <Link className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer' href="/membersonly/introduction">See Intro</Link>
+        <p className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link href="/api/auth/logout">Logout</Link></p>
+        <p className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link href="/membersonly/introduction">See Intro</Link></p>
         </div> </div></div>
     )
   }
