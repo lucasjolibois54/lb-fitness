43c8de8bc8f044babc1516758a27072ab194f5ec
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 15:40:17 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function featuredBlogs() {
   return (
     <div className='my-16 max-w-7xl mx-auto px-6'>
- <h3 className='!leading-tight mb-10 text-off-white text-center px-5 mt-28 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
+ <h3 className='!leading-tight mb-10 text-off-white text-center px-5 mt-16 sm:mt-20 text-4xl xsm:text-3xl sm:text-4xl md:4xl md-lg:text-4xl lg:text-4xl font-bold stroke-removal'>
             FEATURED BLOGS
         </h3>
 
