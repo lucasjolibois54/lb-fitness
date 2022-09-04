31dc5187500f0f1fb19cfadbb206868a759e5d76
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 22:56:45 +0200

    Create nav.js

---
+++ components/main/nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function nav() {
+  return (
+    <>
+    <div className='h-screen w-screen bg-green-900'>
+        <div className='max-w-7xl mx-auto pt-36 text-center'>
+            <h3 className='text-3xl'>
+                Home
+            </h3>
+            <h3 className='text-3xl'>
+                about
+            </h3>
+            <h3 className='text-3xl'>
+                blog
+            </h3>
+        </div>
+    </div></>
+  )
+}
+
+export default nav
