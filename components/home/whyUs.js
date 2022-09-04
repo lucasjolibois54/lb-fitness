1b9e452b6df1ac2b8d6a1830dbc6c6a8d1c7107e
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 13:51:39 +0200

    Update whyUs.js

--- components/home/whyUs.js
+++ components/home/whyUs.js
@@ -11,7 +11,7 @@ function whyUs() {
             <div className="text-center md:text-left mx-5 md:mx-0 mb-10 md:mb-0 flex flex-col justify-center">
                 <h2 className="hidden sm:block text-white mt-10 sm:mt-0 text-5xl xsm:text-5xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY SHOULD <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> YOU CHOOSE US  </span></h2>
                 <h2 className="sm:hidden text-white mt-0 sm:mt-0 text-5xl xsm:text-5xl sm:text-5xl md:lg:text-6xl lg:text-7xl font-bold stroke-removal">WHY TO <span className="stroke-title text-4xl xsm:text-5xl sm:text-4xl md-lg:text-5xl lg:text-6xl"><br /> CHOOSE US  </span></h2>
-                <p className="sm:hidden text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
+                <p className=" text-base md:text-lg py-5 lg:px-24 lg:pl-0 lg:pr-20 max-w-2xl text-gray-400 mt-5">
                 LB Fitness is the best free-to-use fitness platform available on the internet, containing workout &amp; diet plans, fitness related blogs, quizzes to help you achieve your goals, and so much more. Sign up to get started!                </p>
                 <div className="mt-4">
                     <Link  href="/api/auth/login"><span className="text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer">
