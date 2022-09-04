3dd359da8b587079b6222285098ed9305afa48ef
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 21:02:41 +0200

    Update subHero.js

--- components/meals/subHero.js
+++ components/meals/subHero.js
@@ -3,10 +3,45 @@ import React from 'react'
 function subHero() {
   return (
     <>
-    <div className='max-w-4xl mx-auto'>
-            <div className='mt-48 3xl:mt-28 mr-7 ml-7'>
-    <h1 className='sm:ml-7 max-w-4xl text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-medium '>What to find here</h1>
-    <p className='md:text-lg sm:ml-7 sm:mt-8 max-w-3xl mt-6 text-left text-gray-subtext font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>    </div>
+
+    <div className='max-w-5xl mx-auto mt-36'>
+    <div className='e mt-5 px-5'>
+        <h3 className='!leading-tight text-center mt-4 text-white text-4xl xsm:text-4xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold stroke-removal'>EXPLORE ALL OUR FREE <br/> RECEPIES AND DIET PLANS</h3>
+        <p className="text-base text-center font-light md:text-lg py-5 lg:px-24 mx-auto max-w-2xl text-gray-400 mt-0">
+        LB Fitness is still in the beta release, so if you&apos;re interested in following along as well as being updated whenever we add new features. Make sure to give us a follow <a href="https://www.instagram.com/lbfitness.eu/" target="_blank" rel="noreferrer" className='text-pink'>@lbfitness.eu</a> and join the community.  </p>
+    </div>
+
+
+
+    <div className="mt-5 sm:mt-0 mb-20  mx-auto px-5 flex flex-col sm:flex-row sm:space-x-14">
+        <div className="w-full mx-auto my-5 sm:w-4/12 space-y-4">
+          
+        <a href="https://www.instagram.com/lbfitness.eu/" rel="noreferrer" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            alt="Image" className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661531340/Web%20Dev%20Projects/Rectangle_745_1_xhum5q.webp"
+          />
+          </div></a>
+        </div>
+
+        <div className="w-full mx-auto my-5 sm:w-4/12 space-y-4">
+        <a href="https://www.instagram.com/lbfitness.eu/" rel="noreferrer" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            alt="Image" className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661538003/Web%20Dev%20Projects/Rectangle-745-_2_-_2__otmc4x.webp"
+          />
+          </div></a>
+        </div>
+
+        <div className="w-full mx-auto my-5 sm:w-4/12 space-y-4">
+        <a href="https://www.instagram.com/lbfitness.eu/" rel="noreferrer" target="_blank"><div className="transition duration-300 group transform hover:-translate-y-3 hover:shadow-2xl rounded-2xl cursor-pointer"><img
+            alt="Image" className="planimg"
+            src="https://res.cloudinary.com/dckwf6med/image/upload/v1661538104/Web%20Dev%20Projects/Rectangle-745-_3_-_1__qxgmvy.webp"
+          />
+          </div></a>
+        </div>
+      </div>
+
+
+   
     </div>
     </>
   )
