fe34c3515f6c8aa2bc78e6d1c8f7a30fb4156126
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 16:59:14 +0200

    Create subHero.js

---
+++ components/meals/subHero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function subHero() {
+  return (
+    <>
+    <div className='max-w-4xl mx-auto'>
+            <div className='mt-48 3xl:mt-28 mr-7 ml-7'>
+    <h1 className='sm:ml-7 max-w-4xl text-left !leading-tight mt-4 text-white text-4xl xsm:text-3xl sm:text-4xl md:6xl md-lg:text-5xl lg:text-6xl font-medium '>What to find here</h1>
+    <p className='md:text-lg sm:ml-7 sm:mt-8 max-w-3xl mt-6 text-left text-gray-subtext font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>    </div>
+    </div>
+    </>
+  )
+}
+
+export default subHero
