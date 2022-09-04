2bef0295914f90e274438393cdf73cee39a67815
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 16:59:10 +0200

    Update meals.js

--- pages/membersonly/meals.js
+++ pages/membersonly/meals.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/meals/hero'
 import HomeHeader from '../../components/main/homeHeader'
+import SubHero from '../../components/meals/subHero'
 
 function meals() {
   return (
     <div className='h-screen'>
       <HomeHeader/>
       <Hero/>
+      <SubHero/>
     </div>
   )
 }
