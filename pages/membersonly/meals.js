3bc2f3036c97cbb6d72815d23377969c59ccf789
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 20:47:44 +0200

    Update meals.js

--- pages/membersonly/meals.js
+++ pages/membersonly/meals.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../../components/meals/hero'
 import HomeHeader from '../../components/main/homeHeader'
 import SubHero from '../../components/meals/subHero'
+import Footer from '../../components/main/footer'
 
 function meals() {
   return (
@@ -9,6 +10,7 @@ function meals() {
       <HomeHeader/>
       <Hero/>
       <SubHero/>
+      <Footer/>
     </div>
   )
 }
