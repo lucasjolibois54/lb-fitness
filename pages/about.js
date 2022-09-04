add44aa3cb288bfc526b839518b8e22c5e171c77
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-04 22:59:12 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ import AboutVideo from '../components/about/aboutVideo';
 function about() {
   return (
     <div className=''>
-        <Nav/>
+        {/*<Nav/>*/}
         <HomeHeader/>
         <TempHero/>
         <AboutVideo/>
