47303b6897a3e5bc2fe063be3d7dc242c349e292
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 18:33:09 +0200

    Update headerAfterLogin.js

--- components/main/headerAfterLogin.js
+++ components/main/headerAfterLogin.js
@@ -10,9 +10,9 @@ function headerAfterLogin() {
         <p className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link href="/api/auth/logout">Logout</Link></p>
         <p className='float-right mx-7 -mb-7 mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link href="/membersonly/introduction">See Intro</Link></p>
         </div> </div></div>*/
-        <div className=' zzzz max-w-7xl mx-auto font-bold border-white/20 sm:border-none border-b'>
+        <div className=' zzz zzzz max-w-7xl mx-auto font-bold border-white/20 sm:border-none border-b'>
         <div className="block">
-        <p href="/" className='z-50 text-center float-left text-2xl mx-7 sm:mr-20 -mb-7 mt-7 sm:mt-12  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
+        <p href="/" className='z-50 text-center float-left text-2xl mx-7 sm:mr-20 -mb-7 mt-7 sm:mt-12 ml-5  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
         <div className="block">
         <p className='z-50 float-right mx-7 ml-0 mr-16 sm:mr-20 -mb-7 mt-5 sm:mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/logout">Logout</Link></p>
         <Nav/>
