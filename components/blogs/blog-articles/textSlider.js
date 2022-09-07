c7fd85506899991d8647d88e02106983d7814d07
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 23:40:20 +0200

    Update textSlider.js

--- components/blogs/blog-articles/textSlider.js
+++ components/blogs/blog-articles/textSlider.js
@@ -6,7 +6,7 @@ function textSlider(props) {
                     <div className="mt-20 sm:pb-12">
 
 <div className="whitespace-nowrap">
-    <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">{props.text}
+    <p className="overscroll-on-text-over-x-axis flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">{props.text}
     </p>
 </div>
 
