8cb20959970703aa238e78d92f24967098a7ed70
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 21:26:43 +0200

    Update blogs.js

--- pages/blogs.js
+++ pages/blogs.js
@@ -3,8 +3,15 @@ import HomeHeader from '../components/main/homeHeader';
 import Hero from '../components/blogs/hero'
 import FeaturedBlogs from '../components/blogs/featuredBlogs'
 import Footer from '../components/main/footer';
+import { useUser } from '@auth0/nextjs-auth0';
 
 function blogs() {
+  const { user, error, isLoading } = useUser();
+  /*console.log(user)*/
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div className='h-screen'>
       <HomeHeader/>
