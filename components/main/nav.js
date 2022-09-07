1b0efca745bce6a0d9f7af6215ffa4ea508ff963
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 20:23:58 +0200

    Update nav.js

--- components/main/nav.js
+++ components/main/nav.js
@@ -11,15 +11,30 @@ function nav() {
     </label>
 
     <ul className="menu__box">
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/">Home</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/about">About us</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/api/auth/login">Login &amp; Signup</Link></p></li>
-      <li><p className="menu__item" ><Link className="cursor-pointer" href="/membersonly/introduction">Introduction</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/api/auth/login">Plan Overview</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/blogs">Blog Overview</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/about/#faq-section">FAQ&apos;S</Link></p></li>
-      <li><p className="menu__item" ><Link className=" cursor-pointer" href="/membersonly/meals">Meal Plans</Link></p></li>
 
+        {/* phone */}
+      <div className='text-left sm:text-center mt-10 ml-7 space-y-5 sm:space-y-0 sm:hidden'>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/">Home</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/api/auth/login">Login &amp; Signup</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className="cursor-pointer" href="/membersonly/introduction">Introduction</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/about">About us</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/api/auth/login">Plan Overview</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/blogs">Blog Overview</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/about/#faq-section">FAQ&apos;S</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/membersonly/meals">Meal Plans</Link></p></li>
+      </div>
+
+      {/* pc */}
+      <div className='text-left sm:text-center mt-10 ml-7 space-y-5 sm:space-y-0 hidden sm:block'>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/">Home</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/api/auth/login">Login &amp; Signup</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className="cursor-pointer" href="/membersonly/introduction">Introduction</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/about">About us</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/api/auth/login">Plan Overview</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/blogs">Blog Overview</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/about/#faq-section">FAQ&apos;S</Link></p></li>
+      <li><p className="navhover transition ease-in-out delay-10 hover:-translate-y-1 hover:scale-110 duration-300 !leading-tight sm:pb-5 text-white text-3xl xsm:text-4xl sm:text-5xl md-lg:text-7xl lg:text-6xl font-bold " ><Link className=" cursor-pointer" href="/membersonly/meals">Meal Plans</Link></p></li>
+      </div>
     </ul>
   </div>
 </>
