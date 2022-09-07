019c0c864cdb9872bb4e623172fa8715b512ea20
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 23:24:20 +0200

    Update subHero.js

--- components/meals/subHero.js
+++ components/meals/subHero.js
@@ -7,7 +7,7 @@ function subHero() {
 
     <div className='max-w-5xl mx-auto mt-36'>
     <div id='subHeroFood' className='e mt-5 px-5'>
-        <h3 className='!leading-tight text-center mt-4 text-white text-4xl xsm:text-4xl pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold stroke-removal'>EXPLORE ALL OUR FREE <br/> RECEPIES AND DIET PLANS</h3>
+        <h3 className='!leading-tight text-center mt-4 text-white text-4xl xsm:text-3xl md:pr-7 sm:text-4xl md:6xl md-lg:text-5xl lg:text-5xl font-bold stroke-removal'>EXPLORE ALL OUR FREE <br/> RECEPIES AND DIET PLANS</h3>
         <p className="text-base text-center font-light md:text-lg py-5 lg:px-24 mx-auto max-w-2xl text-gray-400 mt-0">
         LB Fitness is still in the beta release, so if you&apos;re interested in following along as well as being updated whenever we add new features. Make sure to give us a follow <a href="https://www.instagram.com/lbfitness.eu/" target="_blank" rel="noreferrer" className='text-pink'>@lbfitness.eu</a> and join the community.  </p>
     </div>
