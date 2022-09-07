c0c8b71fc6345cad852dd079cd741eb95b412d27
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 18:41:09 +0200

    Update featuredBlogs.js

--- components/blogs/featuredBlogs.js
+++ components/blogs/featuredBlogs.js
@@ -12,9 +12,9 @@ function featuredBlogs() {
 <div className="mx-auto">
   <div className="grid sm:grid-cols-3 gap-2 sm:grid-flow-row grid-">
     <div className="p-5 sm:p-2 text-center bg-transparent sm:col-start-1 sm:col-end-2">
-    <img alt="Image" className='w-full' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/>
-    <p className='mt-5 text-left text-gray-subtext text-sm px-2 font-extralight'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its.</p>
-    <p className='mt-2 text-left text-pink text-sm px-2 font-extralight'>Read more</p>
+    <Link href='/articles/benefitsOfHIIT'><img alt="Image" className='w-full cursor-pointer' src='https://res.cloudinary.com/dckwf6med/image/upload/v1661808957/Web%20Dev%20Projects/image_61_1_k3629a.webp'/></Link>
+    <p className='mt-5 text-left text-gray-subtext text-sm px-2 font-extralight'>HIIT training is a perfect way not only to burn calories, but also to burn fat. After your workout, all of that intense ...</p>
+    <Link href='/articles/benefitsOfHIIT'><p className='cursor-pointer mt-2 text-left text-pink text-sm px-2 font-extralight'>Read more</p></Link>
     </div>
 
     <div className="p-5 sm:p-2 text-center bg-transparent  sm:col-start-2 sm:col-end-4">
