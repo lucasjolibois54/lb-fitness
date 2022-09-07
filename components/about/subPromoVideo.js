8a1b6bb306cc2bba7ff69863c705e1dfe192a53c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-07 23:41:12 +0200

    Update subPromoVideo.js

--- components/about/subPromoVideo.js
+++ components/about/subPromoVideo.js
@@ -6,7 +6,7 @@ function subPromoVideo() {
     <div className="mt-0 pb-20">
 
         <div className="whitespace-nowrap">
-            <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-7xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
+            <p className="overscroll-on-text-over-x-axis flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-7xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
             </p>
         </div>
 
