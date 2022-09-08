2a06d91951b5a4beb4be963ba0bb29a6479103c2
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-09 00:36:13 +0200

    Create homeTestPage.js

---
+++ pages/homeTestPage.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+const LoadingComponent = () => <div>Loading...</div>;
+
+export default function HomeLoadingTest() {
+    const [isLoading, setLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    const onLoadEffect = () => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+        setTimeout(() => {
+            setIsError(true);
+        }, 10000);
+    };
+
+    useEffect(onLoadEffect, []);
+
+    if (isLoading) {
+        return <LoadingComponent />;
+    }
+    return (
+        <div className="App">
+            {isError ? (
+                <div style={{ color: "red" }}>Something went wrong</div>
+            ) : (
+                <div>Data that you want to display</div>
+            )}
+        </div>
+    );
+}
