37dbe167ae625b80a8223f294643bf17126d6115
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-08 18:27:54 +0200

    update

--- components/home/textSlider.js
+++ components/home/textSlider.js
@@ -2,10 +2,10 @@
 function textSlider() {
     return (
         <div>
-            <div className="mt-10 sm:mt-20 pb-20">
+            <div className="mt-10 sm:mt-20 pb-20 overscroll-on-text-over-x-axis">
 
                 <div className="whitespace-nowrap">
-                    <p className="overscroll-on-text-over-x-axis flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-6xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
+                    <p className="flex marqueeStyle text-white mt-7 sm:mt-0 text-6xl xsm:text-6xl sm:text-6xl md-lg:text-7xl lg:text-8xl font-bold stroke-title">FREE WORKOUT PLAN AT SIGN-UP FREE WORKOUT PLAN AT SIGN-UP
                     </p>
                 </div>
 
