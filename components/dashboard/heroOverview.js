db70b0aa71347818c2d58520a9370ad2ec917e4e
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-08 12:29:35 +0200

    Update heroOverview.js

--- components/dashboard/heroOverview.js
+++ components/dashboard/heroOverview.js
@@ -13,7 +13,7 @@ export default function HeroOverview() {
     <div className='max-w-7xl mx-auto'>
     {/* <button onClick={toggleAccordion} className={`bg-red-500 ${state}`}>HEYYO</button> */}
               {/* more coming soon */}
-              <h2 className=" !leading-tight pb-10 text-white text-center mt-20 pt-20 sm:mt-36 text-4xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-6xl font-bold stroke-removal">
+              <h2 className=" !leading-tight pb-10 text-white text-center mt-10 pt-20 sm:mt-36 text-4xl xsm:text-4xl sm:text-6xl md-lg:text-7xl lg:text-6xl font-bold stroke-removal">
               WELCOME TO THE OVERVIEW PAGE{" "}
         <span className=" text-2xl xsm:text-2xl sm:text-4xl md-lg:text-5xl lg:text-6xl stroke-title">
           <br />
