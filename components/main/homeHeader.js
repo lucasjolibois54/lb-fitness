212d588e891703f55a33af5fcc71363112ed789d
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-11 00:17:24 +0200

    Update homeHeader.js

--- components/main/homeHeader.js
+++ components/main/homeHeader.js
@@ -35,7 +35,7 @@ const Header = () => {      // variable with the component Header. () => means i
 
     <div className=' zzz zzzz max-w-7xl mx-auto font-bold border-white/20 sm:border-none border-b'>
     <div className="block">
-    <p href="/" className='z-50 text-center float-left text-xl xsm:text-xl semixsm:text-2xl semixsm:mx-7 sm:mr-20  -mb-7 mt-7 sm:mt-12 semixsm:ml-5 ml-4  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
+    <p href="/" className='z-50 text-center float-left text-xl xsm:text-xl semixsm:text-2xl semixsm:mx-7 sm:mr-20  -mb-7 mt-6 sm:mt-12 semixsm:ml-5 ml-4  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
     <div className="block">
     <p className='z-50 float-right mx-7 ml-0 mr-16 sm:text-lg text-sm semixsm:text-md semixsm:mr-16 sm:mr-20 -mb-7 mt-5 sm:mt-12 text-pink py-2 px-5 xxsm:px-10 semixsm:px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/login">Login</Link></p>
     <Nav/>
