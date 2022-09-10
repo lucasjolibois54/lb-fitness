9e6086a76a2e6d5863374c5c0b7bc0749dda42c5
lucasjolibois54 <lucasjolibois@icloud.com>
2022-09-11 00:08:06 +0200

    Update headerAfterLogin.js

--- components/main/headerAfterLogin.js
+++ components/main/headerAfterLogin.js
@@ -12,9 +12,9 @@ function headerAfterLogin() {
         </div> </div></div>*/
         <div className=' zzz zzzz max-w-7xl mx-auto font-bold border-white/20 sm:border-none border-b'>
         <div className="block">
-        <p href="/" className='z-50 text-center float-left text-2xl mx-7 sm:mr-20 -mb-7 mt-7 sm:mt-12 ml-5  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
+        <p href="/" className='z-50 text-center float-left text-lg xsm:text-xl semixsm:text-2xl mx-7 sm:mr-20  -mb-7 mt-7 sm:mt-12 semixsm:ml-5 ml-4  sm:text-3xl  pb-4 sm:pb-0'><Link href="/">LB│FITNESS</Link></p>
         <div className="block">
-        <p className='z-50 float-right mx-7 ml-0 mr-16 sm:mr-20 -mb-7 mt-5 sm:mt-12 text-pink py-2 px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/logout">Logout</Link></p>
+        <p className='z-50 float-right mx-7 ml-0 mr-16 text-sm semixsm:text-md sm:text-lg semixsm:mr-16 sm:mr-20 -mb-7 mt-5 sm:mt-12 text-pink py-2 px-5 xxsm:px-10 semixsm:px-10 rounded-full border-gray-stroke border-2 cursor-pointer'><Link  href="/api/auth/logout">Logout</Link></p>
         <Nav/>
         </div> </div></div>
     )
